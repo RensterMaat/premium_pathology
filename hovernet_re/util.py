6bca1eb72ae5abc007f8c57ebc88d4441a386fbc
Rens ter Maat <rens.termaat@gmail.com>
2022-07-14 16:04:17 +0200

    hovernet reverse engineering

---
+++ hovernet_re/util.py
@@ -0,0 +1,123 @@
+import sys
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from math import ceil
+from collections import OrderedDict
+
+sys.path.insert(1, '/home/rens/repos/hover_net')
+from models.hovernet.post_proc import __proc_np_hv
+from misc.utils import get_bounding_box
+
+def extract_patch(xmin, xmax, ymin, ymax):
+    size_x = ceil((xmax-xmin) / 164) * 164 + 92
+    size_y = ceil((ymax-ymin) / 164) * 164 + 92
+
+    patch = np.array(img.read_region(
+        (xmin-46,ymin-46), 0, (size_x,size_y)
+    ))
+
+    return patch  
+
+def format_output(out):
+    # format np
+    pred_dict = OrderedDict(
+        [[k, v.permute(0, 2, 3, 1).contiguous()] for k, v in out.items()]
+    )
+    pred_dict['np'] = F.softmax(pred_dict['np'], dim=-1)[...,1:]
+
+    # format tp
+    type_map = F.softmax(pred_dict["tp"], dim=-1)
+    type_map = torch.argmax(type_map, dim=-1, keepdim=True)
+    type_map = type_map.type(torch.float32)
+    pred_dict["tp"] = type_map
+
+    # to single array
+    pred_output = torch.cat(list(pred_dict.values()), -1).numpy()
+
+    return pred_output
+
+
+
+
+def output2annotations(pred_output):
+    pred_map = pred_output
+    nr_types = 6
+    return_centroids = False
+
+    if nr_types is not None:
+        pred_type = pred_map[..., :1]
+        pred_inst = pred_map[..., 1:]
+        pred_type = pred_type.astype(np.int32)
+    else:
+        pred_inst = pred_map
+
+    pred_inst = np.squeeze(pred_inst)
+    pred_inst = __proc_np_hv(pred_inst)
+
+    inst_info_dict = None
+    if return_centroids or nr_types is not None:
+        inst_id_list = np.unique(pred_inst)[1:]  # exlcude background
+        inst_info_dict = {}
+        for inst_id in inst_id_list:
+            inst_map = pred_inst == inst_id
+            # TODO: chane format of bbox output
+            rmin, rmax, cmin, cmax = get_bounding_box(inst_map)
+            inst_bbox = np.array([[rmin, cmin], [rmax, cmax]])
+            inst_map = inst_map[
+                inst_bbox[0][0] : inst_bbox[1][0], inst_bbox[0][1] : inst_bbox[1][1]
+            ]
+            inst_map = inst_map.astype(np.uint8)
+            inst_moment = cv2.moments(inst_map)
+            inst_contour = cv2.findContours(
+                inst_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+            )
+            # * opencv protocol format may break
+            inst_contour = np.squeeze(inst_contour[0][0].astype("int32"))
+            # < 3 points dont make a contour, so skip, likely artifact too
+            # as the contours obtained via approximation => too small or sthg
+            if inst_contour.shape[0] < 3:
+                continue
+            if len(inst_contour.shape) != 2:
+                continue # ! check for trickery shape
+            inst_centroid = [
+                (inst_moment["m10"] / inst_moment["m00"]),
+                (inst_moment["m01"] / inst_moment["m00"]),
+            ]
+            inst_centroid = np.array(inst_centroid)
+            inst_contour[:, 0] += inst_bbox[0][1]  # X
+            inst_contour[:, 1] += inst_bbox[0][0]  # Y
+            inst_centroid[0] += inst_bbox[0][1]  # X
+            inst_centroid[1] += inst_bbox[0][0]  # Y
+            inst_info_dict[inst_id] = {  # inst_id should start at 1
+                "bbox": inst_bbox,
+                "centroid": inst_centroid,
+                "contour": inst_contour,
+                "type_prob": None,
+                "type": None,
+            }
+
+    if nr_types is not None:
+        #### * Get class of each instance id, stored at index id-1
+        for inst_id in list(inst_info_dict.keys()):
+            rmin, cmin, rmax, cmax = (inst_info_dict[inst_id]["bbox"]).flatten()
+            inst_map_crop = pred_inst[rmin:rmax, cmin:cmax]
+            inst_type_crop = pred_type[rmin:rmax, cmin:cmax, 0]
+            inst_map_crop = (
+                inst_map_crop == inst_id
+            )  # TODO: duplicated operation, may be expensive
+            inst_type = inst_type_crop[inst_map_crop]
+            type_list, type_pixels = np.unique(inst_type, return_counts=True)
+            type_list = list(zip(type_list, type_pixels))
+            type_list = sorted(type_list, key=lambda x: x[1], reverse=True)
+            inst_type = type_list[0][0]
+            if inst_type == 0:  # ! pick the 2nd most dominant if exist
+                if len(type_list) > 1:
+                    inst_type = type_list[1][0]
+            type_dict = {v[0]: v[1] for v in type_list}
+            type_prob = type_dict[inst_type] / (np.sum(inst_map_crop) + 1.0e-6)
+            inst_info_dict[inst_id]["type"] = int(inst_type)
+            inst_info_dict[inst_id]["type_prob"] = float(type_prob)
+
+    return inst_info_dict
