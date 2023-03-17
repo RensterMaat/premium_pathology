2f47d58f06ee5c0baf100bb37215ad056c98b6c6
RensterMaat <rens.termaat@gmail.com>
2023-03-17 13:55:02 +0100

    heatmaps + attention model
    Former-commit-id: d68fca1f3f1dddc1937191dc87b0a41358ec2ebd
    Former-commit-id: 1f4811551febfba8edd8f781afac65f3dce5655e
    Former-commit-id: 11f511d0b5ff3ca8dd1a16a431bc18f579c2ed9e [formerly ec3c6aa1390f9d31b80107ae0da28e6aea11fdf9]
    Former-commit-id: 6c17108f9508c03192f9ec964a0fcc8208cd2f91

--- hipt_feature_extractor/extract.py
+++ hipt_feature_extractor/extract.py
@@ -1,12 +1,23 @@
+import os
 import torch
+import yaml
+import sys
 import h5py
+import argparse
 import numpy as np
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from hipt_model_utils import get_vit256
+from tqdm import tqdm
 from pathlib import Path
 from openslide import OpenSlide
+
+with open(Path(__file__).parent / "config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+sys.path.append(str(Path(config["hipt_repository_path"]) / "HIPT_4K"))
+
+from hipt_4k import HIPT_4K
 
 
 class PatchDataset(Dataset):
@@ -24,13 +35,16 @@ class PatchDataset(Dataset):
 
 
 class HIPTFeatureExtractor(pl.LightningModule):
-    def __init__(self, slide_path):
+    def __init__(self, slide_path, patches_path, output_dir=None, patch_size=4096):
         super().__init__()
+
         self.case_id = slide_path.stem
-        self.slide = OpenSlide(slide_path)
+        self.slide = OpenSlide(str(slide_path))
+        self.patch_coordinates = h5py.File(patches_path, "r")["coords"]
 
-        self.model = get_vit256(CHECKPOINT_PATH)
-
+        self.output_dir = output_dir
+        self.patch_size = patch_size
+        self.model = HIPT_4K()
         self.embeddings = []
 
     def forward(self, x):
@@ -42,19 +56,16 @@ class HIPTFeatureExtractor(pl.LightningModule):
 
     def test_epoch_end(self, *args):
         all_embeddings = torch.concatenate(self.embeddings)
-        torch.save(all_embeddings, FEATURE_DIR / (self.case_id + ".pt"))
+        torch.save(all_embeddings, Path(self.output_dir) / (self.case_id + ".pt"))
 
     def setup(self, stage="test"):
-        patches_path = (
-            PATCH_DIR / "metastasis" / center / "patches" / (self.case_id + ".h5")
-        )
-        patch_coordinates = h5py.File(patches_path, "r")["coords"]
-
         transform = transforms.Compose(
             [
                 transforms.Lambda(
                     lambda coords: np.array(
-                        self.slide.read_region(coords, 1, (PATCH_SIZE, PATCH_SIZE))
+                        self.slide.read_region(
+                            coords, 1, (self.patch_size, self.patch_size)
+                        )
                     )[:, :, :-1]
                 ),
                 transforms.ToTensor(),
@@ -62,37 +73,59 @@ class HIPTFeatureExtractor(pl.LightningModule):
             ]
         )
 
-        self.dataset = PatchDataset(patch_coordinates, transform)
+        self.dataset = PatchDataset(self.patch_coordinates, transform)
 
     def test_dataloader(self):
-        return DataLoader(self.dataset, batch_size=64, shuffle=False, num_workers=24)
+        return DataLoader(self.dataset, batch_size=1, shuffle=False, num_workers=12)
 
 
-PATHOLOGY_ROOT = Path("/mnt/hpc/pathology")
+def extract_patches(slide_dir, patch_dir, output_dir):
+    for slide_path in list(Path(slide_dir).iterdir()):
+        case_id = slide_path.stem
+        print(case_id)
+        patches_path = Path(patch_dir) / (case_id + ".h5")
 
-WSI_DIR = PATHOLOGY_ROOT / "metastasis"
-PATCH_DIR = PATHOLOGY_ROOT / "hipt_preprocessed"
-FEATURE_DIR = PATHOLOGY_ROOT / "hipt_features/primary_vs_metastasis"
+        if not patches_path.exists():
+            print(f"No patch coordinate file found for {case_id}")
+            continue
 
-CHECKPOINT_PATH = (
-    "/home/rens/repos/premium_pathology/hipt/checkpoints/vit256_small_dino.pth"
+        fe = HIPTFeatureExtractor(slide_path, patches_path, output_dir)
+
+        trainer = pl.Trainer(accelerator="gpu")
+        trainer.test(fe)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--slide_dir",
+    type=str,
+    default=config["slide_dir"],
+    help="Directory containing WSI slides for extraction.",
+)
+parser.add_argument(
+    "--preprocess_dir",
+    type=str,
+    default=config["preprocess_dir"],
+    help="Directory containing the preprocessing results of the WSIs to process. Should have the same structure as wsi_dir, except for the last layer which should contain the masks, patches and stitches subfolders",
+)
+parser.add_argument(
+    "--output_dir",
+    type=str,
+    default=config["feature_dir"],
+    help="Directory for saving the output feature vectors.",
 )
 
-PATCH_SIZE = 256
 
-center = "radboud"
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-for slide_path in sorted(list((WSI_DIR / center).iterdir()))[19:]:
-    print(slide_path.stem)
+    for root, _, files in os.walk(args.slide_dir):
+        sub_dir = root.replace(args.slide_dir, "")
+        if sub_dir and sub_dir[0] == "/":
+            sub_dir = sub_dir[1:]
 
-    patches_path = (
-        PATCH_DIR / "metastasis" / center / "patches" / (slide_path.stem + ".h5")
-    )
-
-    if not patches_path.exists():
-        continue
-
-    fe = HIPTFeatureExtractor(slide_path)
-    trainer = pl.Trainer(accelerator="gpu")
-
-    trainer.test(fe)
+        patches_dir = Path(args.preprocess_dir) / sub_dir / "patches"
+        if files:
+            print("Extracting features from slides in:", root)
+            print("Using patch coordinates in:", patches_dir)
+            extract_patches(root, patches_dir, args.output_dir)
---
+++ hipt_feature_extractor/heatmap.py
@@ -0,0 +1,92 @@
+import torch.nn as nn
+import numpy as np
+from einops import rearrange, repeat
+from scipy.ndimage import zoom
+from tqdm.notebook import tqdm
+from torchvision import transforms
+
+
+def get_unnormalized_last_selfattention(model, x):
+    x = model.prepare_tokens(x)
+    for i, block in enumerate(model.blocks):
+        if i < len(model.blocks) - 1:
+            x = block(x)
+        else:
+            attention_layer = block.attn
+
+            B, N, C = x.shape
+            qkv = (
+                attention_layer.qkv(x)
+                .reshape(
+                    B, N, 3, attention_layer.num_heads, C // attention_layer.num_heads
+                )
+                .permute(2, 0, 3, 1, 4)
+            )
+            q, k, v = qkv[0], qkv[1], qkv[2]
+
+            attn = (q @ k.transpose(-2, -1)) * attention_layer.scale
+
+            return attn
+
+
+def extract_attention(feature_extractor, x):
+    x = x.unsqueeze(0)
+    batch_256, w_256, h_256 = feature_extractor.model.prepare_img_tensor(x)
+    batch_256 = batch_256.unfold(2, 256, 256).unfold(3, 256, 256)
+    batch_256 = rearrange(batch_256, "b c p1 p2 w h -> (b p1 p2) c w h")
+    batch_256 = batch_256.to(feature_extractor.model.device256, non_blocking=True)
+    features_cls256 = feature_extractor.model.model256(batch_256)
+
+    attention_256 = feature_extractor.model.model256.get_last_selfattention(batch_256)
+    nh = attention_256.shape[1]  # number of head
+    attention_256 = attention_256[:, :, 0, 1:].reshape(256, nh, -1)
+    attention_256 = attention_256.reshape(w_256 * h_256, nh, 16, 16)
+    attention_256 = (
+        nn.functional.interpolate(attention_256, scale_factor=16, mode="nearest")
+        .cpu()
+        .numpy()
+    )
+    attention_256 = rearrange(attention_256, "(b1 b2) c h w -> c (b1 h) (b2 w)", b1=16)
+
+    features_grid256 = (
+        features_cls256.reshape(w_256, h_256, 384)
+        .transpose(0, 1)
+        .transpose(0, 2)
+        .unsqueeze(dim=0)
+    )
+    features_grid256 = features_grid256.to(
+        feature_extractor.model.device4k, non_blocking=True
+    )
+
+    # attention_4k = fe.model.model4k.get_last_selfattention(features_grid256)
+    attention_4k = get_unnormalized_last_selfattention(
+        feature_extractor.model.model4k, features_grid256
+    )
+    nh = attention_4k.shape[1]  # number of head
+    attention_4k = attention_4k[0, :, 0, 1:].reshape(nh, -1)
+    attention_4k = attention_4k.reshape(nh, w_256, h_256)
+    attention_4k = (
+        nn.functional.interpolate(
+            attention_4k.unsqueeze(0), scale_factor=256, mode="nearest"
+        )[0]
+        .cpu()
+        .numpy()
+    )
+
+    return attention_256, attention_4k
+
+
+def transform_image(x):
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=0.5, std=0.5),
+        ]
+    )
+
+    return transform(x[:, :, :-1])
+
+
+def extract_single_image_attention(image_4k):
+    transformed_image = transform_image(image_4k)
+    return extract_attention(transformed_image)
